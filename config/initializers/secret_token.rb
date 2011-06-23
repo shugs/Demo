d713fa27e2f24b6c23dd61e3db350e1a91b4fe1b
davidhughes <d.hughes@ifad.org>
2011-06-23 16:05:27 +0200

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '6db916e43df7c45c7c25f2547240c117013a0ce05d140e45cd26a04a86b48a66fd9947609ac279cb33380deabb531f286b5dc5cd160eb0b21ae6deed7d6731d7'
